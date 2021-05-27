3fecd20c438b5c29cb7ec8de6caffd39764c5fb3
darby5mcnally <darby5mcnally@gmail.com>
2021-05-27 14:03:25 -0600

    display forecast data using API

--- src/script.js
+++ src/script.js
@@ -21,34 +21,49 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForcast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForcast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
   
-  let days = ["Thursday", "Friday", "Saturday", "Sunday"];
-  
   let forecastHTML = `<div class="row">`;
-  days.forEach(function(day) {
-    forecastHTML = forecastHTML +
-    `
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
+    forecastHTML = 
+      forecastHTML +
+      `
       <div class="col-2">
         <div class="weather-forecast-date">
-          ${day}
+          ${formatDay(forecastDay.dt)}
         </div>
         <img 
-          src="http://openweathermap.org/img/wn/50d@2x.png" 
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperature">
           <span class="weather-forecast-temperature-max">
-            <b> 35°</b>
+            <b> ${
+              Math.round(forecastDay.temp.max)
+            }°</b>
           </span> | 
           <span class="weather-forecast-temperature-min">
-            18°
+            ${
+              Math.round(forecastDay.temp.min)
+            }°
           </span>
         </div>
       </div>
   `;
+    }
   });
  
   forecastHTML = forecastHTML + `</div>`;
@@ -68,6 +83,13 @@ function search(city) {
   let apiKey = "abba93708591ea289439caf0b26bbc06";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showTemperature);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "abba93708591ea289439caf0b26bbc06";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForcast);
+
 }
 
 function showTemperature(response) {
@@ -93,8 +115,9 @@ function showTemperature(response) {
   );
   iconElement.setAttribute(
     "alt",
-    response.data.weather[0].description
-  );
+    response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 }
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", cityForm);
@@ -142,4 +165,3 @@ let celciusLink = document.querySelector("#celcius");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("Salt Lake City");
-displayForcast();
